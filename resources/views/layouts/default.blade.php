8007e77ecaf6ee455b1c67d473c7937e7dc1d4f1
Asier Callejo <acallejo@bilbomatica.es>
2021-09-24 19:35:52 +0200

    Fixed default.blade video tag

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -88,7 +88,10 @@
             </div>
         </div>
         <div id="video-intro">
-            <video playsinline type="video/mp4" preload="auto" autoplay loop muted class="fillWidth fadeIn wow collapse in" data-wow-delay="0.5s" poster="{{ asset('images/posiciones-karate.jpg') }}" id="video-background" src="{{ asset('videos/intro-video.mp4') }}" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeIn;"></video>
+            <video playsinline preload="auto" autoplay loop muted class="fillWidth fadeIn wow in" data-wow-delay="0.5s" poster="{{ asset('images/posiciones-karate.jpg') }}" id="video-background" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeIn;">
+                <source src="{{ URL::asset('videos/intro-video.mp4') }}" type="video/mp4" >
+                Your browser does not support the video tag. 
+            </video>
         </div>
     </body>
 </html>

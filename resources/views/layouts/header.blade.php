e0f90f55545409c524b872ce7fbd48de3956d35f
Cristian Bodón <bodon@todologic.com>
2021-11-05 23:05:52 +0100

    Few fixes for the intro, circle buttons sections started

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,13 +1,20 @@
-<div class="flex-center position-ref full-height container">
-    <div>
-        <li style="display:inline-flex; float:right; margin-right:2em;">
-            <a href="{{ route('change_lang', ['lang' => 'es']) }}"> <img src='{{ asset('images/idiomas/castellano.png') }}' class='img-responsive ' style='margin-right: 6px; margin-top:0.2em; height:29px; width: 30px;' /></a>
-            <a href="{{ route('change_lang', ['lang' => 'en']) }}"><img src='{{ asset('images/idiomas/ingles.png') }}' class='img-responsive' style=' margin-right: 6px; height:35px; width: 30px;' /></a>
-            <a href="{{ route('change_lang', ['lang' => 'eu']) }}"><img src='{{ asset('images/idiomas/euskera.png') }}' class='img-responsive' style=' margin-right: 6px; height:35px; width: 30px;' /></a>
+<nav>
+    <div class="logo">
+        <img src="{{ URL::asset('images/logo.png') }}">
+    </div>
+    <input type="checkbox" id="click">
+    <label for="click" class="menu-btn">
+        <i class="fas fa-bars"></i>
+    </label>
+    <ul>
+        <li><a class="active" href="{{ route('index') }}">@lang('lang.'.$titulo)</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li class="lang">
+            <a href="{{ route('change_lang', ['lang' => 'es']) }}"> <img src='{{ asset('images/idiomas/castellano.png') }}' /></a>
+            <a href="{{ route('change_lang', ['lang' => 'en']) }}"><img src='{{ asset('images/idiomas/ingles.png') }}' /></a>
+            <a href="{{ route('change_lang', ['lang' => 'eu']) }}"><img src='{{ asset('images/idiomas/euskera.png') }}' /></a>
         </li>
-    </div>
-
-    <div class="links">
-        <a href="{{ route('index') }}">@lang('lang.'.$titulo)</a>
-    </div>
-</div>
+    </ul>
+</nav>
--- resources/views/sections/circles.blade.php
+++ resources/views/sections/circles.blade.php
@@ -1,3 +1,24 @@
-    <div class="title m-b-md">
-        circles
+<div id="circle-section" class="container-fluid">
+    <div class="row">
+        <div class="col-lg-3 col-md-6">
+            <div class='button-image'>
+                <div class='text'>Karate</div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class='button-image'>
+                <div class='text'>Kick Boxing</div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class='button-image'>
+                <div class='text'>Pilates</div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class='button-image'>
+                <div class='text'>Aikido</div>
+            </div>
+        </div>
     </div>
+</div>

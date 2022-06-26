365b2bdaaa5f4213ef8ae6ecb4a31c2f73c296d4
acallejp_97 <acallejp@gmail.com>
2022-06-27 00:41:13 +0200

    Added auto insert images for activityDescription

--- resources/views/layouts/sponsors.blade.php
+++ resources/views/layouts/sponsors.blade.php
@@ -4,8 +4,8 @@
     <h2>Sponsors</h2>
     </div>
 <div class="owl-carousel owl-theme">
-    			 <a target="_blank" href="https://www.facebook.com/autoescuelakatia/"><img src="{{asset('images/sponsors/katia.png')}}"></a>
-			<a target="_blank" href="https://www.bilbotex.com/"><img src="{{asset('images/sponsors/bilbotex.jpg')}}"></a>
+    <a target="_blank" href="https://www.facebook.com/autoescuelakatia/"><img src="{{asset('images/sponsors/katia.png')}}"></a>
+    <a target="_blank" href="https://www.bilbotex.com/"><img src="{{asset('images/sponsors/bilbotex.jpg')}}"></a>
     <a target="_blank" href="https://www.instagram.com/queridamia.shop/"><img src="{{asset('images/sponsors/querida_mia.jpg')}}"></a>
    <a target="_blank" href="https://vehiclescompany.com/"><img src="{{asset('images/sponsors/vehicles_company.jpg')}}"></a>
 </div>

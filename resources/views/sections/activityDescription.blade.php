eb5ad12691dfde639012a410594bda063c59d1c9
Cristian Bodón <bodon@todologic.com>
2022-06-26 21:29:07 +0200

    Merge

--- resources/views/sections/activityDescription.blade.php
+++ resources/views/sections/activityDescription.blade.php
@@ -9,19 +9,9 @@
             @lang('lang.description-'.$titulo.'-infantil')
         @endif
     </p>
-        <div class="row">
-            <div class="col-lg-6">
-    			<img src="{{asset('images/entrada.png')}}" alt="entrada" class="mb-3">
-    		</div>
-            <div class="col-lg-6">
-    			<img src="{{asset('images/entrada.png')}}" alt="entrada" class="mb-3">
-    		</div>
-            <div class="col-lg-6">
-    			<img src="{{asset('images/entrada.png')}}" alt="entrada" class="mb-3">
-    		</div>
-            <div class="col-lg-6">
-    			<img src="{{asset('images/entrada.png')}}" alt="entrada" class="mb-3">
-    		</div>
-		</div>
 	</div>
 
+
+    		<div class="owl-carousel owl-theme">
+    			<img src="{{asset('images/entrada.png')}}" alt="entrada" class="mb-3">
+			</div>
